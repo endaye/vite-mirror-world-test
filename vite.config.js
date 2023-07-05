0256a3fdd7f683b73552bdad80be115749610cf0
Yuancheng Zhang <old.far.hot@gmail.com>
2023-07-05 19:12:12 +0800

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,16 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
+  build: {
+    //rollupOptions: {
+    //  input: 'src/index.html'
+    //},
+    outDir: "build",
+  },
+  optimizeDeps: {},
   server: {
     headers: {
       "Cross-Origin-Embedder-Policy": "require-corp",
